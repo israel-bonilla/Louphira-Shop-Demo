cafcef44a83a1a88339834cd13758852f8400efe
coldthaw <israel1337b@gmail.com>
2022-01-29 10:33:22 -0600

    image format config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["links.israelb.xyz"],
+    formats: ['image/webp'],
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
